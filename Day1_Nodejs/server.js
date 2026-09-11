08320a3eec7d60b6a2e76c78a1e0a20e0b872f14
Sakshee Sachan <sakshee.sachan003@gmail.com>
2026-09-11 15:50:34 +0530

    created first server

---
+++ Day1_Nodejs/server.js
@@ -0,0 +1,18 @@
+// imports http and save in variable
+let http = require("http");
+console.log("HTTP:", http);
+
+// creates server -> accepts callback
+let server = http.createServer((req, res) => {
+  console.log("Hello I'm server");
+
+  // sends response from response
+  res.end("I have listened you....");
+});
+
+// starts server -> listen (needs port)
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// In browser -> http://localhost:3000/
