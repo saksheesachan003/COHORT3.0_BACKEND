0cf7b1b2d298c570485aff8dc0113182ac3f8e07
Sakshee Sachan <sakshee.sachan003@gmail.com>
2026-09-26 15:52:35 +0530

    frontend part of Notes Integration

--- Day6_Notes_Integration/Backend/src/app.js
+++ Day6_Notes_Integration/Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 const notesRoute = require("./routes/notes.route");
 
 const app = express();
@@ -7,6 +8,10 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 app.get("/", (req, res) => {
   res.send("ok got it");
--- Day6_Notes_Integration/Notes_Intregation/src/App.jsx
+++ Day6_Notes_Integration/Notes_Intregation/src/App.jsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import NoteCard from "./components/NoteCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    description: "",
+  });
+  
+  const [updateNoteId, setUpdateNoteId] = useState(null);
+
+  const [allNotes, setAllNotes] = useState([]);
+
+  const handleChange = (e) => {
+    setFormValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  let getAllNotes = async () => {
+    try {
+      let res = await axios.get("http://localhost:3000/notes/allNotes");
+      // console.log(res);
+      setAllNotes(res.data.data);
+    } catch (error) {
+      console.log("error in get all notes api", error);
+    }
+  };
+
+  useEffect(() => {
+    getAllNotes();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (updateNoteId) {
+      // api call for update note
+      let res = await axios.put(
+        `http://localhost:3000/notes/${updateNoteId}`,
+        formValues,
+      );
+      console.log(res);
+      setUpdateNoteId(null);
+    } else {
+      // api call for create note
+      let res = await axios.post(
+        "http://localhost:3000/notes/create",
+        formValues,
+      );
+      console.log(res);
+    }
+
+    setFormValues({
+      title: "",
+      description: "",
+    });
+    getAllNotes();
+  };
+
+  let deleteNote = async (id) => {
+    try {
+      let res = await axios.delete(`http://localhost:3000/notes/${id}`);
+      console.log(res);
+      getAllNotes();
+    } catch (error) {
+      console.log("error in delete note", error);
+    }
+  };
+
+  let noteForUpdate = (note) => {
+    console.log(note);
+    setUpdateNoteId(note._id);
+    setFormValues({
+      title: note.title,
+      description: note.description,
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-screen p-5 flex flex-col gap-5">
+      <h1 className="text-3xl font-semibold">Notes app</h1>
 
-export default App
+      <form
+        onSubmit={handleSubmit}
+        className="w-70 border gap-5 border-white p-4 rounded-xl flex flex-col"
+      >
+        <input
+          onChange={handleChange}
+          name="title"
+          value={formValues.title}
+          className="p-2 outline-none text-xl rounded border border-white"
+          type="text"
+          placeholder="Title"
+        />
+        <input
+          onChange={handleChange}
+          name="description"
+          value={formValues.description}
+          className="p-2 outline-none text-xl rounded border border-white"
+          type="text"
+          placeholder="Description"
+          minLength={20}
+          required
+        />
+        <button className="bg-blue-600 text-white p-2 rounded">
+          {updateNoteId ? "Update note" : "Add note"}
+        </button>
+      </form>
+
+      <div className="flex gap-4 flex-wrap">
+        {allNotes.map((val) => (
+          <NoteCard
+            key={val._id}
+            note={val}
+            noteForUpdate={noteForUpdate}
+            deleteNote={deleteNote}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
